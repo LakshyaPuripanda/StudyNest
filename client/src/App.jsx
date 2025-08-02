18f6af396cd0431eb79be95d7a881ab7646fb804
SaiHarshitaG <hariganti2003@gmail.com>
2025-08-02 16:29:20 +0530

    Admin Course Related

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import Sidebar from './pages/admin/Sidebar'
 import Dashboard from './pages/admin/Dashboard'
 import CourseTable from './pages/admin/course/CourseTable'
 import AddCourse from './pages/admin/course/AddCourse'
+import EditCourse from './pages/admin/course/EditCourse'
 
 const appRouter = createBrowserRouter([
   {
@@ -66,7 +67,11 @@ const appRouter = createBrowserRouter([
           {
             path:"course/create",
             element: <AddCourse/>
-          }
+          },
+          {
+            path:"course/:courseId",
+            element:<EditCourse/>
+          },
         ]
       }
     ],
---
+++ client/src/pages/admin/course/CourseTab.jsx
@@ -0,0 +1,182 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import RichTextEditor from '@/components/ui/RichTextEditor';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const CourseTab = () => {
+    const [input, setInput] = useState({
+        courseTitle: "",
+        subTitle: "",
+        description: "",
+        category: "",
+        courseLevel: "",
+        coursePrice: "",
+        courseThumbnail: ""
+    });
+    const navigate = useNavigate();
+    const changeEventHandler = (e) => {
+        const { name, value } = e.target;
+        setInput({ ...input, [name]: value });
+    };
+
+    const selectCategory = (value) =>{
+        setInput ({...input, category:value});
+    }
+
+    const selectCourseLevel = (value) =>{
+        setInput ({...input, courseLevel:value});
+    }
+
+    //get file
+    const selectThumbnail = (e) => {
+        const file = e.target.files?.[0];
+        if (file){
+            setInput({...input, courseThumbnail:file});
+        }
+    }
+    const isPublished = false;
+    const isLoading = false;
+    return (
+        <Card>
+            <CardHeader className="flex flex-row justify-between">
+                <div>
+                    <CardTitle>Basic Course Information</CardTitle>
+                    <CardDescription>
+                        Make changes to your courses here. CLick save when you are done.
+                    </CardDescription>
+                </div>
+                <div className="space-x-2">
+                    <Button variant='outline'>
+                        {isPublished ? "Unpulished" : "Publish"}
+                    </Button>
+                    <Button>
+                        Remove Course
+                    </Button>
+                </div>
+            </CardHeader>
+            <CardContent>
+                <div className="space-y-4 mt-5">
+                    <div>
+                        <Label>Course Title</Label>
+                        <Input
+                            type="text"
+                            name="CourseTitle"
+                            value={input.courseTitle}
+                            onChange={changeEventHandler}
+                            placeholder="Ex. Fullstack Developer" />
+                    </div>
+                    <div>
+                        <Label>Subtitle</Label>
+                        <Input
+                            type="text"
+                            name="SubTitle"
+                            value={input.subTitle}
+                            onChange={changeEventHandler}
+                            placeholder="Ex. Become a Fullstack developer from zero to hero " />
+                    </div>
+                    <div>
+                        <Label>Description</Label>
+                        <RichTextEditor input={input} setInput={setInput} />
+                    </div>
+                    <div className="flex items-center gap-5">
+                        <div>
+                            <Label>Category</Label>
+                            <Select onValueChange={selectCategory}>
+                                <SelectTrigger className="w-[180px]">
+                                    <SelectValue placeholder="Select a category" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Category</SelectLabel>
+                                        <SelectItem value="Next JS">Next JS</SelectItem>
+                                        <SelectItem value="Data Science">Data Science</SelectItem>
+                                        <SelectItem value="Frontend Development">
+                                            Frontend Development
+                                        </SelectItem>
+                                        <SelectItem value="Fullstack Development">
+                                            Fullstack Development
+                                        </SelectItem>
+                                        <SelectItem value="MERN Stack Development">
+                                            MERN Stack Development
+                                        </SelectItem>
+                                        <SelectItem value="Javascript">Javascript</SelectItem>
+                                        <SelectItem value="Python">Python</SelectItem>
+                                        <SelectItem value="Docker">Docker</SelectItem>
+                                        <SelectItem value="MongoDB">MongoDB</SelectItem>
+                                        <SelectItem value="HTML">HTML</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div>
+                            <Label>Course Level</Label>
+                            <Select onValueChange = {selectCourseLevel}>
+                                <SelectTrigger className="w-[180px]">
+                                    <SelectValue placeholder="Select a Course Level" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Course Level</SelectLabel>
+                                        <SelectItem value="Beginner">Beginner</SelectItem>
+                                        <SelectItem value="Medium">Medium</SelectItem>
+                                        <SelectItem value="Advance">Advance</SelectItem>
+
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div>
+                            <Label>Price in (INR)</Label>
+                            <Input
+                                type="number"
+                                name="coursePrice"
+                                value={input.coursePrice}
+                                onChange={changeEventHandler}
+                                placeholder="199"
+                                className="w-fit"
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <Label>Course Thumbnail</Label>
+                        <Input
+                            type="file"
+                            accept="image/*"
+                            className="w=fit"
+                        />
+                    </div>
+                    <div>
+                        <Button onClick={() => navigate("/admin/course")} variant="outline">Cancel</Button>
+                        <Button disabled ={isLoading}>
+                            {
+                                isLoading ? (
+                                    <>
+                                    <Loader2 className ="mr-2 h-4 w-4 animate-spin"/>
+                                    Please wait
+                                    </>
+                                ) : (
+                                    "Save"
+                                )}
+                        </Button>
+                    </div>
+
+                </div>
+            </CardContent>
+        </Card>
+
+    )
+}
+
+export default CourseTab
